0b9470e959a945ec5c8ced67a88c8e4430c998dc
SHARIEF <imadsharf@live.com>
2020-07-06 02:13:30 +0530

    Added Components and Services

--- angular-crud/src/app/app.module.ts
+++ angular-crud/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrudComponent } from './crud/crud.component';
+import { CrudDetailComponent } from './crud/crud-detail/crud-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrudComponent,
+    CrudDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-crud/src/app/crud/crud-detail/crud-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-detail',
+  templateUrl: './crud-detail.component.html',
+  styleUrls: ['./crud-detail.component.scss']
+})
+export class CrudDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
